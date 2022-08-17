1fa110904d3a58109ff22b4213084c478f095c9c
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-08-17 23:07:45 +0300

    Added logic for Todo component

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { ChangeEvent, FC, useState } from "react";
 
 const DEFAULT_TODO = {
     name: '',
@@ -7,18 +7,26 @@ const DEFAULT_TODO = {
 
 const Todo: FC = () => {
     const [todos, setTodos] = useState(DEFAULT_TODO)
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setTodos({...todos, [name]: value});
+        console.log(todos)
+    }
+    
+
     return (
         <div>
             <div>
                 <label htmlFor="">
                     <div>Name</div>
-                    <input type="text" name="" id="" />
+                    <input type="text" name="name" id="" onChange={onChange} />
                 </label>
             </div>
             <div>
             <label htmlFor="">
                     <div>Description</div>
-                    <input type="text" name="" id="" />
+                    <input type="text" name="name" id="" onChange={onChange}/>
                 </label>
             </div>
         </div>
